8c5d91a8a2e9a8989f83e1204e4bcb16fe50f509
Kieran <kieran.webdevelopment@gmail.com>
2023-06-08 16:24:31 +0100

    routes created and data retrieved successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import './server';
+import Planets from './Pages/Planets';
 
 function App() {
   return (
     <>
       <h1>blank project</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="/earth" />} />
+
+          <Route path="/:id" element={<Planets />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
