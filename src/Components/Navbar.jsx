89040c9d2150689ce1e0859858250b06921f18b4
Kieran <kieran.webdevelopment@gmail.com>
2023-06-13 15:44:09 +0100

    tablet navbar complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,59 +7,87 @@ import styled from 'styled-components';
 const Header = styled.div`
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   background-color: var(--background-color);
-`;
-const NavHeading = styled.h2`
-  font-size: 1.5rem;
-  letter-spacing: var(--small-line-spacing);
-  font-weight: 400;
-`;
 
+  @media (min-width: 850px) {
+  }
+`;
 const HeaderContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
   padding: 0 1em;
   max-width: 1600px;
   margin: 0 auto;
+  @media (min-width: 850px) {
+    padding: 0 2em;
+    justify-content: space-between;
+  }
 `;
+const NavHeading = styled.h2`
+  font-weight: 400;
+  @media (min-width: 500px) {
+    flex-basis: 100%;
+    text-align: center;
+    padding: 1.5em 0 0.5em 0;
+  }
+
+  @media (min-width: 850px) {
+    flex-basis: auto;
+    font-size: 1.5rem;
+    letter-spacing: var(--small-line-spacing);
+    padding: 0;
+  }
+`;
+
 const Ul = styled.ul`
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  list-style-type: none;
-  gap: 1em;
+  @media (min-width: 550px) {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+    gap: 1em;
+  }
 `;
 
 const Li = styled.li`
-  height: 100%;
-  box-sizing: border-box;
-  border-top: 5px solid transparent;
-  min-width: 70px;
-  text-align: center;
-  opacity: 0.75;
-  &:hover {
-    border-top: 5px solid red;
-    border-top-color: ${(props) => `var(${props.linkColor})`};
-    opacity: 1;
+  @media (min-width: 550px) {
+    height: 100%;
+    box-sizing: border-box;
+    border-top: 5px solid transparent;
+    min-width: 70px;
+    text-align: center;
+    opacity: 0.75;
+
+    &:hover,
+    &:focus,
+    &:active {
+      border-top: 5px solid red;
+      border-top-color: ${(props) => `var(${props.linkcolor})`};
+      opacity: 1;
+    }
   }
 `;
-const StyledLink = styled(Link)`
-  padding: 2em 0;
-  ${'' /* padding-top: calc(2em - 5px); */}
-  text-decoration: none;
-  display: block;
-  width: 100%;
-  height: 100%;
-  color: inherit;
-  font-family: var(--text-font);
-  font-size: 14px;
-  letter-spacing: 1px;
-  line-height: var(--standard-line-height);
-  font-weight: 500;
 
-  &:hover {
-    ${'' /* color: orange; */}
-    opacity: 1;
+const StyledLink = styled(Link)`
+  @media (min-width: 550px) {
+    padding: 2em 0;
+    ${'' /* padding-top: calc(2em - 5px); */}
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+    color: inherit;
+    font-family: var(--text-font);
+    font-size: 14px;
+    letter-spacing: 1px;
+    line-height: var(--standard-line-height);
+    font-weight: 500;
+
+    &:hover {
+      ${'' /* color: orange; */}
+      opacity: 1;
+    }
   }
 `;
 export default function Navbar() {
@@ -69,28 +97,28 @@ export default function Navbar() {
         <NavHeading>THE PLANETS</NavHeading>
         <nav>
           <Ul>
-            <Li linkColor={'--mercury'}>
+            <Li linkcolor={'--mercury'}>
               <StyledLink to="/mercury">MERCURY</StyledLink>
             </Li>
-            <Li linkColor={'--venus'}>
+            <Li linkcolor={'--venus'}>
               <StyledLink to="/venus">VENUS</StyledLink>
             </Li>
-            <Li linkColor={'--earth'}>
+            <Li linkcolor={'--earth'}>
               <StyledLink to="/earth">EARTH</StyledLink>
             </Li>
-            <Li linkColor={'--mars'}>
+            <Li linkcolor={'--mars'}>
               <StyledLink to="/mars">MARS</StyledLink>
             </Li>
-            <Li linkColor={'--jupiter'}>
+            <Li linkcolor={'--jupiter'}>
               <StyledLink to="/jupiter">JUPITER</StyledLink>
             </Li>
-            <Li linkColor={'--saturn'}>
+            <Li linkcolor={'--saturn'}>
               <StyledLink to="/saturn">SATURN</StyledLink>
             </Li>
-            <Li linkColor={'--uranus'}>
+            <Li linkcolor={'--uranus'}>
               <StyledLink to="/uranus">URANUS</StyledLink>
             </Li>
-            <Li linkColor={'--neptune'}>
+            <Li linkcolor={'--neptune'}>
               <StyledLink to="/neptune">NEPTUNE</StyledLink>
             </Li>
           </Ul>
