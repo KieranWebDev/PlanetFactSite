6a4eff931a354501c522b2271ae44ea82cda1913
Kieran <kieran.webdevelopment@gmail.com>
2023-06-15 15:44:51 +0100

    cleaned up Planets Component

--- src/Pages/Planets.jsx
+++ src/Pages/Planets.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+// components
 import PlanetsPageContainer from '../Components/PlanetPage/PlanetsPageContainer';
 import TitleAndInfoContainer from '../Components/PlanetPage/TitleAndInfoContainer';
 import InfoButtonsContainer from '../Components/PlanetPage/InfoButtonsContainer';
 import PlanetImage from '../Components/PlanetPage/PlanetImage';
 import StatisticsContainer from '../Components/PlanetPage/StatisticsContainer';
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 // temporary imports - images will be fetched from api
 import MercuryPlanet from '../assets/planet-mercury.svg';
@@ -34,11 +35,6 @@ import NeptunePlanet from '../assets/planet-neptune.svg';
 import NeptuneInternal from '../assets/planet-neptune-internal.svg';
 import NeptuneGeology from '../assets/geology-neptune.png';
 
-const IMG = styled.img`
-  height: 100px;
-  width: 100px;
-`;
-
 export default function Planets() {
   const [planetInfo, setPlanetInfo] = useState(null);
   const [planetImages, setPlanetImages] = useState({});
@@ -52,12 +48,7 @@ export default function Planets() {
       .then((data) => {
         setPlanetInfo(data);
         setPlanetImages(getPicsForDisplay(data));
-        // console.log(changeDisplayedInfo('overview', 'planetFull'));
-        // changeDisplayedInfo('overview', 'planetFull');
-        // return data;
       });
-    // .then((data) => {
-    // });
   }, [params.id]);
 
   useEffect(() => {
@@ -125,8 +116,6 @@ export default function Planets() {
   // console.log(planetImages);
 
   function changeDisplayedInfo(info, pic) {
-    let textToDisplay = '';
-
     let planetDecriptionText = '';
     let planetWikiLink = '';
     let picToDisplay = '';
@@ -136,29 +125,12 @@ export default function Planets() {
       planetDecriptionText = planetInfo[info].content;
       planetWikiLink = planetInfo[info].source;
     }
-    // <>
-    //   <p> - {planetInfo[info].content}</p>
-    //   <p>
-    //     Source -
-    //     <a target="_blank" rel="noreferrer" href={planetInfo[info].source}>
-    //       Wikipedia
-    //     </a>
-    //   </p>
-    // </>;
+
     if (info === 'geology') {
       picToDisplay = planetImages.planetFull;
       additionalSurfacePic = planetImages[pic];
-
-      // (
-      //   <>
-      //     <img src={planetImages[pic]} alt={planetInfo.name} />
-      //     <img src={planetImages.planetFull} alt={planetInfo.name} />
-      //   </>
-      // );
     } else {
       picToDisplay = planetImages[pic];
-
-      // <IMG src={planetImages[pic]} alt={planetInfo.name} />;
     }
     setDisplayedInfo({
       planetDecriptionText: planetDecriptionText,
@@ -166,32 +138,8 @@ export default function Planets() {
       picToDisplay: picToDisplay,
       additionalSurfacePic: additionalSurfacePic,
     });
-    //  text: textToDisplay, pic: picToDisplay
   }
-  // else {
-  //   textToDisplay = (
-  //     <>
-  //       <p>overview - {planetInfo.overview.content}</p>
-  //       <p>
-  //         Source -{' '}
-  //         <a
-  //           target="_blank"
-  //           rel="noreferrer"
-  //           href={planetInfo.overview.source}
-  //         >
-  //           Wikipedia
-  //         </a>
-  //       </p>
-  //     </>
-  //   );
 
-  //   picToDisplay = (
-  //     <img src={planetImages.planetFull} alt={planetInfo.name} />
-  //   );
-  // }
-
-  //   const picString = `..${planetInfo.images.planet.substring(1)}`;
-  //   console.log(picString);
   return (
     <>
       {planetInfo ? (
@@ -200,38 +148,14 @@ export default function Planets() {
             planetName={planetInfo.name}
             displayedInfo={displayedInfo}
           />
-          <div>
-            {/* <h1>{planetInfo.name}</h1> */}
-            {/* <p>overview - {planetInfo.overview.content}</p>
-          <p>
-            Source -{' '}
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={planetInfo.overview.source}
-            >
-              Wikipedia
-            </a>
-          </p> */}
-            {/* {displayedInfo.text} */}
-            <InfoButtonsContainer changeDisplayedInfo={changeDisplayedInfo} />
 
-            <PlanetImage
-              displayedInfo={displayedInfo}
-              planetName={planetInfo.name}
-            />
-            <StatisticsContainer planetInfo={planetInfo} />
+          <InfoButtonsContainer changeDisplayedInfo={changeDisplayedInfo} />
 
-            {/* <p>rotation- {planetInfo.rotation}</p>
-            <p>radius- {planetInfo.radius}</p>
-            <p>revolution- {planetInfo.revolution}</p>
-            <p>temp- {planetInfo.temperature}</p>
-            <p>{`${planetInfo.name}Planet`}</p> */}
-            {/* {displayedInfo.pic} */}
-            {/* <img src={planetImages.planetFull} alt={planetInfo.name} />
-          <img src={planetImages.planetInternal} alt={planetInfo.name} />
-          <img src={planetImages.planetGeology} alt={planetInfo.name} /> */}
-          </div>
+          <PlanetImage
+            displayedInfo={displayedInfo}
+            planetName={planetInfo.name}
+          />
+          <StatisticsContainer planetInfo={planetInfo} />
         </PlanetsPageContainer>
       ) : (
         <div>Loading...</div>
